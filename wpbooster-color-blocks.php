4c4c3f018b16abedc3fdead4d63fa09eb0db6e2e
Eugen <pustojack@list.ru>
2021-01-31 23:41:53 +0200

    Update wpbooster-color-blocks.php

--- wpbooster-color-blocks.php
+++ wpbooster-color-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Booster: Color Blocks
  * Description: Gorgeous colored blocks for your posts. Do not reduce scores in the PageSpeed test.
- * Version: 1.4
+ * Version: 1.5
  * Author: seojacky 
  * Author URI: https://t.me/big_jacky 
  * GitHub Plugin URI: https://github.com/seojacky/wpbooster-color-blocks
@@ -140,8 +140,8 @@ add_filter( 'style_loader_tag', 'wpb_color_blocks_change_attribute', 10, 2 );
 function wpb_color_blocks_change_attribute($link, $handle) {	
 
 	  if( $handle === 'wpbcb-editor-css' ) {
-        $link = str_replace( 'all', 'print', $link );
+        $link = str_replace( '\'all\'', '\'print\'', $link );
 		$link = str_replace( '/>', 'onload="this.media=\'all\'" />', $link );
     }
     return $link;
-}	
+}
