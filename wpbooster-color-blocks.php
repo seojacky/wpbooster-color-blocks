e243ba01480f1c87d07c414e461612d5416e2dec
Eugen <pustojack@list.ru>
2021-02-05 09:29:11 +0200

    Update wpbooster-color-blocks.php

--- wpbooster-color-blocks.php
+++ wpbooster-color-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Booster: True Color Blocks
  * Description: Gorgeous colored blocks for your posts. Do not reduce scores in the PageSpeed test.
- * Version: 1.5
+ * Version: 1.6
  * Author: seojacky 
  * Author URI: https://t.me/big_jacky 
  * GitHub Plugin URI: https://github.com/seojacky/wpbooster-color-blocks
